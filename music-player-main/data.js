3ab101388b0eb4a39a3def40a697938c573d2f80
Pulkit Joshi <75159916+pulkitxb@users.noreply.github.com>
2022-07-27 23:30:55 +0530

    Add files via upload

---
+++ music-player-main/data.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+        name: 'Apocalypse',
+        path: 'assets/musics/Song 1.mp3',
+        artist: 'Midranger',
+        cover: 'assets/images/cover 1.png'
+    },
+    {
+        name: 'On & On',
+        path: 'assets/musics/Song 2.mp3',
+        artist: 'Cartoon Feat. Danial Levi',
+        cover: 'assets/images/cover 2.png'
+    },
+    {
+        name: 'Hand In Glove',
+        path: 'assets/musics/Song 3.mp3',
+        artist: 'Anc D',
+        cover: 'assets/images/cover 3.png'
+    },
+    {
+        name: 'The Dazzled Moon',
+        path: 'assets/musics/Song 4.mp3',
+        artist: 'Lone Lover',
+        cover: 'assets/images/cover 4.png'
+    },
+    {
+        name: 'Like Me',
+        path: 'assets/musics/Song 5.mp3',
+        artist: 'T-Mass & Britt Lari',
+        cover: 'assets/images/cover 5.png'
+    },
+    {
+        name: 'So Happy',
+        path: 'assets/musics/Song 6.mp3',
+        artist: 'Briskit',
+        cover: 'assets/images/cover 6.png'
+    },
+    {
+        name: 'Mortals',
+        path: 'assets/musics/Song 7.mp3',
+        artist: 'Warriyo Feat. Laira Brehm',
+        cover: 'assets/images/cover 7.png'
+    },
+    {
+        name: 'Safe & Sound',
+        path: 'assets/musics/Song 8.mp3',
+        artist: 'Deaf Kev & Sendi Hoxha',
+        cover: 'assets/images/cover 8.png'
+    },
+]
